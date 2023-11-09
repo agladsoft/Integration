18ae23db8c6bf38c6939d1040ba64ff67e5378cc
uvens <uventus1992@gmail.com>
2023-11-09 17:36:19 +0300

    add wait future_list

--- crm.sh
+++ crm.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 path=/home/softit/Integration_crm
+cd path
+source ${path}/venv/bin/activate
 while true;
 do
-  cd path
-  source ${path}/venv/bin/activate
 	python3 ${path}/run.py;
 	sleep 30;
 done
