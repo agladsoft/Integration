26a14a7608aae4953b239084b78c2ab0869200f9
uvens <uventus1992@gmail.com>
2024-01-06 16:55:32 +0300

    fixed run time with 60 sec to 600 sec

--- crm.sh
+++ crm.sh
@@ -14,12 +14,17 @@
 path=/home/softit/Integration_crm
 cd $path
 source ${path}/venv/bin/activate
+
 while true; do
     truncate -s 0 ${path}/nohup.out
-    python3 ${path}/run.py;
+    python3 ${path}/run.py
+
     if [ $? -ne 0 ]; then
-        echo "Error executing python3. Exiting..."
-        break
+        echo "Error executing python3. Restarting..."
+    else
+        echo "Python script executed successfully."
     fi
+
+    # Ожидание 10 минут перед следующим запуском
     sleep 600
 done
